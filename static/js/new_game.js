a6d34126fee23a88f2a28c3771e021c558d41679
Pht <phili2709@gmail.com>
2024-07-09 18:48:17 +0200

    form reset implemented

--- static/js/new_game.js
+++ static/js/new_game.js
@@ -205,4 +205,78 @@ document.getElementById('save-game').addEventListener('click', () => {
             form.reset();
         }
     });
+
+    // Reset the team type radio buttons
+    let team1Types = document.getElementsByName('team1-type');
+    for (let i = 0; i < team1Types.length; i++) {
+        team1Types[i].checked = false;
+    }
+    // Reset the extra points checkboxes
+    let team1ExtraPoints = document.getElementsByName('team1-extra-points');
+    for (let i = 0; i < team1ExtraPoints.length; i++) {
+        team1ExtraPoints[i].checked = false;
+    }
+    // Reset the special conditions checkboxes
+    let team1SpecialConditions = document.getElementsByName('team1-special-conditions');
+    for (let i = 0; i < team1SpecialConditions.length; i++) {
+        team1SpecialConditions[i].checked = false;
+    }
+
+    // Reset the team type radio buttons
+    let team2Types = document.getElementsByName('team2-type');
+    for (let i = 0; i < team2Types.length; i++) {
+        team2Types[i].checked = false;
+    }
+    // Reset the extra points checkboxes
+    let team2ExtraPoints = document.getElementsByName('team2-extra-points');
+    for (let i = 0; i < team2ExtraPoints.length; i++) {
+        team2ExtraPoints[i].checked = false;
+    }
+    // Reset the special conditions checkboxes
+    let team2SpecialConditions = document.getElementsByName('team2-special-conditions');
+    for (let i = 0; i < team2SpecialConditions.length; i++) {
+        team2SpecialConditions[i].checked = false;
+    }
+
+    // Reset the team type radio buttons
+    let team3Types = document.getElementsByName('team3-type');
+    for (let i = 0; i < team3Types.length; i++) {
+        team3Types[i].checked = false;
+    }
+    // Reset the extra points checkboxes
+    let team3ExtraPoints = document.getElementsByName('team3-extra-points');
+    for (let i = 0; i < team3ExtraPoints.length; i++) {
+        team3ExtraPoints[i].checked = false;
+    }
+    // Reset the special conditions checkboxes
+    let team3SpecialConditions = document.getElementsByName('team3-special-conditions');
+    for (let i = 0; i < team3SpecialConditions.length; i++) {
+        team3SpecialConditions[i].checked = false;
+    }
+
+    // Reset the team type radio buttons
+    let team4Types = document.getElementsByName('team4-type');
+    for (let i = 0; i < team4Types.length; i++) {
+        team4Types[i].checked = false;
+    }
+    // Reset the extra points checkboxes
+    let team4ExtraPoints = document.getElementsByName('team4-extra-points');
+    for (let i = 0; i < team4ExtraPoints.length; i++) {
+        team4ExtraPoints[i].checked = false;
+    }
+    // Reset the special conditions checkboxes
+    let team4SpecialConditions = document.getElementsByName('team4-special-conditions');
+    for (let i = 0; i < team4SpecialConditions.length; i++) {
+        team4SpecialConditions[i].checked = false;
+    }
+    // Repeat for Team 2, Team 3, and Team 4
+    // (Implement the same reset logic for other teams here)
+
+    // Reset the round info
+    document.getElementById('game-type').selectedIndex = 0;
+    let winningParty = document.getElementsByName('winning-party');
+    for (let i = 0; i < winningParty.length; i++) {
+        winningParty[i].checked = false;
+    }
+    document.getElementById('points').value = '';
 });
