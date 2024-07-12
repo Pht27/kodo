28208e914b23eccfb6f4af9bd09a1eb77b95ae2c
Pht <phili2709@gmail.com>
2024-07-12 02:46:52 +0200

    gitignore finally gefixt

--- python_scripts/saved_paths.py
+++
@@ -1,9 +0,0 @@
-import os
-
-csv_dir = 'data'
-csv_file_players = os.path.join(csv_dir, 'players.csv')
-csv_file_teams = os.path.join(csv_dir, 'teams.csv')
-csv_file_rounds = os.path.join(csv_dir, 'rounds.csv')
-csv_file_player_team = os.path.join(csv_dir, 'player_is_in_team.csv')
-csv_file_specials = os.path.join(csv_dir, 'specials.csv')
-csv_file_team_round = os.path.join(csv_dir, 'teams_in_round.csv')
