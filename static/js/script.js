7e42b7291e4e9f57b7e54501d43f27053b51626b
Pht <phili2709@gmail.com>
2024-07-12 17:27:48 +0200

    adjusted navbar for mobile

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('navbar-toggle');
+    const navbarMenu = document.getElementById('navbar-menu');
+
+    toggleButton.addEventListener('click', function () {
+        navbarMenu.classList.toggle('show');
+    });
+});
