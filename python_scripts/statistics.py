1141a1c0065c5964c6895e4a593a80420f5738a7
Pht <phili2709@gmail.com>
2024-07-15 02:48:41 +0200

    match history solo bug fix

--- python_scripts/statistics.py
+++ python_scripts/statistics.py
@@ -241,12 +241,27 @@ def get_match_history_infos(specific_player_id=None):
         team4_player2 = 'None'
         if team4_data.shape[0] > 1:
             team4_player2 = team4_data.iloc[1]['name']
+        
+        solos = [   
+                "Trumpfsolo",
+                "Damensolo",
+                "Bubensolo",
+                "Fleischloses",
+                "Knochenloses",
+                "Schlanker Martin",
+                "Kontrasolo",
+                "Stille Hochzeit"
+            ]
 
         # handle won column
+        points = match['points']
+        won = 'None'
         if specific_player_id is not None:
             won = match['won']
-        else:
-            won = 'None'
+            if not won:
+                points = -points
+            if match['game_type'] in solos:
+                points = 3*points
 
         this_round = {'round_id' : match['round_id'],
             'date' : pd.to_datetime(match['date']).strftime("%d %b, %Y, %H:%M:%S"),
@@ -264,7 +279,7 @@ def get_match_history_infos(specific_player_id=None):
             'team4_player2': team4_player2,
             'team4_party' : team4_party,
             'winning_party' : match['winning_party'],
-            'points' : match['points'],
+            'points' : points,
             'won': won
             }
             
@@ -273,89 +288,6 @@ def get_match_history_infos(specific_player_id=None):
         data = pd.concat([data, this_round], ignore_index=True)
     
     data = data.sort_values(by='date', ascending=False)
-    return data
-
-def calc_stats_for_player(player_id):
-    # import data
-    players = pd.read_csv(csv_file_players, index_col=False)
-    players_teams = pd.read_csv(csv_file_player_team, index_col=False)
-    teams = pd.read_csv(csv_file_teams, index_col=False)
-    rounds = pd.read_csv(csv_file_rounds, index_col=False)
-    teams_rounds = pd.read_csv(csv_file_team_round, index_col=False)
-
-    # calc no of team members in round to calculate points
-    team_members = teams.merge(players_teams, on="team_id").groupby(['team_id']).size().reset_index(name='no_team_members')
-
-    # joins teams with players
-    data = players.merge(players_teams, on="player_id").merge(team_members, on='team_id')
-    
-    data = data.merge(teams, on='team_id')
-    
-    data = data.merge(teams_rounds, on='team_id')
-    
-    # merge with rounds
-    data = data.merge(rounds, on='round_id')
-
-    # if given a date, filter up to that date
-    if date is not None:
-        data = data[pd.to_datetime(data['date']) <= date]    
-
-    # check if game was won
-    data.loc[data['party']!=data['winning_party'], 'points'] *= -1
-
-    # check if game was solo, if so triple points for Re party
-    solos = [   
-        "Trumpfsolo",
-        "Damensolo",
-        "Bubensolo",
-        "Fleischloses",
-        "Knochenloses",
-        "Schlanker Martin",
-        "Kontrasolo",
-        "Stille Hochzeit"
-    ]
-
-    data.loc[(data['game_type'].isin(solos)) & (data['party'] == 'Re'), 'points'] *= 3
- 
-    # check if game was won
-    data['won'] = np.where(data['party']==data['winning_party'], True, False)
-
-    # add to count games
-    data['played'] = 1
-
-    # divide points by team members
-    data = data.astype({'points':float})
-    data.loc[:, 'points'] /= data['no_team_members']
-
-    # drop unimportant cols
-    data = data[['player_id', 'name', 'points', 'won', 'played', 'start_points']]
-    
-    # sum over all rows to get wr and points
-    data = data.groupby(['player_id', 'name', 'start_points']).sum().reset_index()
-    data['winrate'] = round(data['won'] / data['played'], 4)
-    data['points'] += data['start_points']
-
-    # drop unimportant cols
-    data = data[['player_id', 'name', 'points', 'winrate']]
-
-    # # if a player has not played a round yet, they will not be shown in the table
-    # # to fix this, we add them with their initial points
-    data = players.merge(data, on=["player_id", "name"], how="left")
-    data.loc[data['points'].isnull(), 'points'] = data['start_points']
-    data.loc[data['winrate'].isnull(), 'winrate'] = -1
-
-    # lastly, drop players who are inactive
-    if filter_active:
-        data = data[data['active']]
-
-    # drop unimportant cols
-    if date is None:
-        data = data[['name', 'points', 'winrate']]
-    else:
-        data = data[['name', 'points', 'winrate', 'player_id']]
-
-    data = data.sort_values(by=['points'],  ascending=False)
-    
     return data
 
 def calc_team_wr_for_player(specific_player_id):
@@ -372,9 +304,6 @@ def calc_team_wr_for_player(specific_player_id):
 
     data = pd.concat([tmp, data], axis=0)
     return data
-
-def calc_player_stats():
-    calc_player_stats_for_specific_player()
 
 def calc_player_stats_for_specific_player(specific_player_id=1, up_to=20):
     # import data
