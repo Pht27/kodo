92343fcf0c28d7e55f6987ae73bc83418fc80efa
Pht <phili2709@gmail.com>
2024-07-30 21:15:06 +0200

    reactivate players added

--- static/js/inactive_players.js
+++ static/js/inactive_players.js
@@ -9,12 +9,37 @@ document.addEventListener('DOMContentLoaded', function () {
                 playerList.innerHTML = '';
                 data.forEach(player => {
                     const li = document.createElement('li');
-                    li.innerHTML = `${player.name}`;
+                    li.classList.add('player-container')
+                    li.innerHTML = `${player.name} <span class="reactivate-player" data-id="${player.player_id}">♻️</span>`;
+                    li.addEventListener('click', function () {
+                        window.location.href = `/player/${player.player_id}`;
+                    });
                     playerList.appendChild(li);
+                });
+
+                // Event Listener für Lösch-Buttons hinzufügen
+                document.querySelectorAll('.reactivate-player').forEach(button => {
+                    button.addEventListener('click', function (event) {
+                        event.stopPropagation(); // Prevent the click from propagating to the li element
+                        if (confirm('Are you sure you want to reactivate this player?')) {
+                            reactivatePlayer(button.getAttribute('data-id'));
+                        }
+                    });
                 });
             });
     }
 
     // Spieler initial laden
     loadPlayers();
+
+    // Spieler löschen
+    function reactivatePlayer(playerId) {
+        fetch(`/api/players/${playerId}`, {
+            method: 'REACTIVATE'
+        }).then(response => {
+            if (response.ok) {
+                loadPlayers();
+            }
+        });
+    }
 });
